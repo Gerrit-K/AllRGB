a1aa7cde89230047ad692fecbbb1e944b997bd42
Gerrit Kieffer <kieffer.gerrit@gmail.com>
2017-04-02 00:16:08 +0200

    added some color distances; results are surprising but less satisfying

--- src/main/java/allrgb/core/ColorDistances.java
+++ src/main/java/allrgb/core/ColorDistances.java
@@ -9,4 +9,10 @@ public interface ColorDistances {
             (c1, c2) -> (c1.getRed() - c2.getRed()) * (c1.getRed() - c2.getRed())
                     + (c1.getGreen() - c2.getGreen()) * (c1.getGreen() - c2.getGreen())
                     + (c1.getBlue() - c2.getBlue()) * (c1.getBlue() - c2.getBlue());
+
+    BiFunction<Color, Color, Double> SQUARED_HUE_DIFFERENCE =
+            (c1, c2) -> (c1.getHue() - c2.getHue()) * (c1.getHue() - c2.getHue());
+
+    BiFunction<Color, Color, Double> SQUARED_BRIGHTNESS_DIFFERENCE =
+            (c1, c2) -> (c1.getBrightness() - c2.getBrightness()) * (c1.getBrightness() - c2.getBrightness());
 }
